1fc9e51a7fdf80675c7c887a828f2de8854e9538
mawais54013 <muhammad.awais@datadoghq.com>
2021-09-27 13:39:30 -0700

    file

---
+++ UnitTest/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  UnitTest
+//
+//  Created by Enrico Piovesan on 09/12/2016.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let ferrari = Car(type: .Sport, transmissionMode: .Drive)
+        ferrari.start(minutes: 120)
+        print(ferrari.miles) // => 140
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+//DD_TEST_RUNNER=1 DD_ENV=ci DD_SERVICE=my-swift-app xcodebuild \
+//-project UnitTest.xcodeproj \
+//-scheme UnitTest \
+//-destination platform=iOS\ Simulator,name=iPhone\ 12\ Pro \
+//test
+
